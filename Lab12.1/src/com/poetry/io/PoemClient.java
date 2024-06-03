ed6e50fd8d6967de44aea1ee1938cea73a8181af
Summer Davis <summerd@tlgcohort.com>
2024-06-03 17:05:17 -0500

    Generate haiku using file reading and writing

--- Lab12.1/src/com/poetry/io/PoemClient.java
+++ Lab12.1/src/com/poetry/io/PoemClient.java
@@ -8,53 +8,62 @@
 
 package com.poetry.io;
 
-import java.io.BufferedReader;
-import java.io.IOException;
+import java.io.*;
 
 public class PoemClient {
 
-    /**
-     * To run one method at a time, uncomment the call to the one you want to execute.
-     */
-    public static void main(String[] args) {
-        // readPoem();
-        // writePoem();
-    }
+  /** To run one method at a time, uncomment the call to the one you want to execute. */
+  public static void main(String[] args) {
+     readPoem();
+    // writePoem();
+  }
 
-    /**
-     * TASK: read all the lines in file 'famous-poem.txt' and print them.
-     *
-     * Avoid the temptation to open the file in the IDE first.
-     * You'll see the poem once you get your code working(!)
-     *
-     * This file is in the module's root directory, which is the current / working
-     * directory at runtime.  Therefore, the path to the file is just the filename.
-     *
-     * Use a BufferedReader wrapped around a FileReader.
-     * The try-with-resources below allows you to initialize the stream and auto-close it.
-     */
-    private static void readPoem() {
-        // TODO: initialize 'reader' variable and complete the try block
-        try (BufferedReader reader = null) {
-
-        }
-        catch (IOException e) {
-            e.printStackTrace();
-        }
+  /**
+   * TASK: read all the lines in file 'famous-poem.txt' and print them.
+   *
+   * <p>Avoid the temptation to open the file in the IDE first. You'll see the poem once you get
+   * your code working(!)
+   *
+   * <p>This file is in the module's root directory, which is the current / working directory at
+   * runtime. Therefore, the path to the file is just the filename.
+   *
+   * <p>Use a BufferedReader wrapped around a FileReader. The try-with-resources below allows you to
+   * initialize the stream and auto-close it.
+   */
+  private static void readPoem() {
+    // initialize 'reader' variable and complete the try block
+    try (BufferedReader reader = new BufferedReader(new FileReader("haiku.txt"))) {
+      String line;
+      while ((line = reader.readLine()) != null) { // when it goes null, you are at EOF
+        System.out.println(line);
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
     }
+  }
 
-    /**
-     * TASK: write a Haiku to file 'haiku.txt'.
-     *
-     * A Haiku is a 3-line poem with the following characteristics:
-     *  1st line is 5 syllables
-     *  2nd line is 7 syllables
-     *  3rd line is 5 syllables
-     *
-     * Use a PrintWriter wrapped around a FileWriter.
-     * Use a try-with-resources to initialize the stream and auto-close it.
-     */
-    private static void writePoem() {
-        // TODO
+  /**
+   * TASK: write a Haiku to file 'haiku.txt'.
+   *
+   * <p>A Haiku is a 3-line poem with the following characteristics:
+   * 1st line is 5 syllables
+   * 2nd line is 7 syllables
+   * 3rd line is 5 syllables
+   *
+   * <p>Use a PrintWriter wrapped around a FileWriter. Use a try-with-resources to initialize the
+   * stream and auto-close it.
+   */
+  private static void writePoem() {
+    try (PrintWriter writer = new PrintWriter(new FileWriter("haiku.txt"))) {
+      writer.println(
+          """
+          if there is one truth
+          for an immutable class
+          there ain't no settuhz
+          """);
     }
+    catch (IOException e) {
+        e.printStackTrace();
+    }
+  }
 }
